203185068a682122b2d503a76b8dbb81da652f13
Nayan Biswas <148667814+mrnayandev@users.noreply.github.com>
2023-12-29 12:41:42 +0000

    build

--- Basics/1_Variables.js
+++ Basics/1_Variables.js
@@ -2,6 +2,7 @@ const accountId = 1248569;
 let accountEmail = "me@gmail.com";
 var accountPassword = "@$#%^1245";
 accountCity = "Chattogram";
-let accountOtp;
+let account;
 
+console.log(account);
 
